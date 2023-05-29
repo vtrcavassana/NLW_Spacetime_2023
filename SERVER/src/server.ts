0a3aa15b11ea9349888973171c6902e74f620cf7
vtrcavassana <victor.cavassana@outlook.com>
2023-05-29 12:07:40 -0300

    Criando rota para listar usuários

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,11 +1,23 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 // Fastify é o Express melhorado
 const app = fastify()
 
+// Variável 'prisma' será usado para fazer conexão com nosso banco
+const prisma = new PrismaClient()
+
 // Rota padrão do nosso aplicativo
-app.get('/', () => {
+app.get('/', async () => {
   return 'Hello World'
+})
+
+// Rota para listar os usuários da aplicação
+// 'async/await' pois é uma conexão com um banco
+app.get('/usuarios', async () => {
+  // 'findMany()' exibe todos os dados da tabela 'usuario'
+  const usuarios = await prisma.usuario.findMany()
+  return usuarios
 })
 
 // Criando a 'porta de entrada' do servidor
