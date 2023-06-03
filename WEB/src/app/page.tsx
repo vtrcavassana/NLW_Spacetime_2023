9d65065599f811754aa420634ee3cc092c882537
vtrcavassana <victor.cavassana@outlook.com>
2023-06-03 17:30:23 -0300

    Criando parte Mobile (EXPO), formatando layout WEB

--- WEB/src/app/page.tsx
+++ WEB/src/app/page.tsx
@@ -1,16 +1,24 @@
-import { Botao } from "@/components/Botao";
+// import { Botao } from '@/components/Botao'
 
 export default function Home() {
   return (
-    <h1>
-      Hello World
-      {/* Componentes - Funções que retornam HTML */}
-      <Botao/>
-      
-      {/* Propriedades é uma forma de passar informações a um Componente */}
-      <Botao titulo='Vitu'/>
-      <Botao titulo='Virtu'/>
-      <Botao titulo='Vi'/>
-    </h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
+
+// export default function Home() {
+//   return (
+//     <h1>
+//       Hello World
+//       {/* Componentes - Funções que retornam HTML */}
+//       {/* <Botao/> */}
+
+//       {/* Propriedades é uma forma de passar informações a um Componente */}
+//       <Botao titulo='Vitu'/>
+//       <Botao titulo='Virtu'/>
+//       <Botao titulo='Vi'/>
+//     </h1>
+//   )
+// }
